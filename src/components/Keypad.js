46b9a179f972ae9d8c676fb45643735d380d95fd
fiona <omburafiona@gmail.com>
2023-09-05 23:57:34 +0300

    event handling lab completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,19 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function security(event){
+console.log ('Entering password...');
+
+    }
     return (
-        <div></div>
+        <div>
+<input 
+type="password"
+onChange={security}
+placeholder="enter password"
+/>
+
+        </div>
     )
 }
 
